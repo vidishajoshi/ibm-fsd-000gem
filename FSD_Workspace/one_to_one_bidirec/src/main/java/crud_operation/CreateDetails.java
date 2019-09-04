b4c92e7ad9f0b734d299dafb60ae728710277859
b4ibmjava05@iiht.tech <b4ibmjava05@iiht.tech>
2019-09-04 10:33:54 +0530

    one to one bidirectional mapping

---
+++ FSD_Workspace/one_to_one_bidirec/src/main/java/crud_operation/CreateDetails.java
@@ -0,0 +1,24 @@
+package crud_operation;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import entity.Instructor;
+import entity.InstructorDetail;
+
+public class CreateDetails {
+public static void main(String args[]) {
+	SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Instructor.class)
+			                  .addAnnotatedClass(InstructorDetail.class).buildSessionFactory();
+	
+	Session session=factory.getCurrentSession();
+			Instructor in=new Instructor("Smith","johnson","smith@g.com");
+	InstructorDetail inD=new InstructorDetail("https://www.youtube.com/artr","Painiting");
+	in.setInstructor_detail(inD);
+	session.beginTransaction();
+	session.persist(in);
+	session.getTransaction().commit();
+	System.out.println("Instructor added ");
+}
+}
