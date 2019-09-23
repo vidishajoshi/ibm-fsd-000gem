997ec05bde66c3d6a4d0e5033453939448a980b5
b4ibmjava05@iiht.tech <b4ibmjava05@iiht.tech>
2019-09-23 21:37:27 +0530

    fitness tracker

--- Spring_Microservices/League_Player_feign_project/src/main/java/com/example/spring/controller/PlayerController.java
+++ Spring_Microservices/League_Player_feign_project/src/main/java/com/example/spring/controller/PlayerController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,15 +16,22 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.spring.dto.CreatePlayerRequest;
 import com.example.spring.service.CommonService;
 
-@RestController
+@Controller
 @RequestMapping("/player")
 public class PlayerController {
 	
 	@Autowired CommonService commonService;
 	
+	@GetMapping("/")
+	public String goHome() {
+		return "pservice/home";
+	}
+	
 	@GetMapping("/view")
-	public List<?> getAllPlayers(){
-		return commonService.getPlayers();
+	public String getAllPlayers(Model theModel){
+		List<?> list= commonService.getPlayers();
+		theModel.addAttribute("player", list);
+	return "pservice/showPlayer";	
 	}
 	
 	
